30a4374d4e778748f5778b667ba41ae6c34fa102
pablolteixeira <xpspablo@hotmail.com>
2023-03-09 20:41:39 -0300

    contest_new function finished

--- substrate-node-unitchain/pallets/contests/src/lib.rs
+++ substrate-node-unitchain/pallets/contests/src/lib.rs
@@ -6,7 +6,9 @@ pub use pallet::*;
 mod tests;
 
 use frame_support::{
-	sp_runtime::FixedPointOperand,
+	sp_runtime::{
+		traits::AccountIdConversion, 
+		FixedPointOperand},
 	PalletId,
 	traits::tokens::{
 		Balance,
@@ -23,8 +25,6 @@ pub mod pallet {
     #[pallet::pallet]
     #[pallet::generate_store(pub(super) trait Store)]
     pub struct Pallet<T>(_);
-
-	pub type AssetIdOf<T> = <T as Config>::AssetId;
 
     #[pallet::config]
     pub trait Config: frame_system::Config {
@@ -71,8 +71,15 @@ pub mod pallet {
 		type MinDescriptionLength: Get<u32>;
 
 		#[pallet::constant]
+		type MinTokenAmount: Get<u32>;
+
+		#[pallet::constant]
 		type MinTokenWinner: Get<u32>;
     }
+
+	pub type AssetBalanceOf<T> = <T as Config>::AssetBalance;
+
+	pub type AssetIdOf<T> = <T as Config>::AssetId;
 
 	#[derive(Clone, Encode, Decode, Eq, PartialEq, RuntimeDebug, Default, MaxEncodedLen, TypeInfo)]
 	#[scale_info(skip_type_params(T))]
@@ -104,7 +111,7 @@ pub mod pallet {
 	#[pallet::event]
 	#[pallet::generate_deposit(pub(super) fn deposit_event)]
 	pub enum Event<T: Config> {
-		ContestCreted { who: T::AccountId }
+		ContestCreted { who: T::AccountId, contest_id: u32, title: BoundedVec<u8, T::MaxTitleLength> }
 	}
 
 	#[pallet::error]
@@ -118,6 +125,8 @@ pub mod pallet {
 		DescriptionTooLarge,
 		DescriptionTooSmall,
 		PrizeTokenWinnerTooSmall,
+		AssetBalanceInsufficient,
+		TokenAmountTooSmall
 	}
 
 	#[pallet::call]
@@ -130,6 +139,7 @@ pub mod pallet {
 			contest_id: u32,
 			title: BoundedVec<u8, T::MaxTitleLength>,
 			prize_token_id: AssetIdOf<T>,
+			prize_token_amount: AssetBalanceOf<T>,
 			prize_token_winner: u32,
 			token_symbol: BoundedVec<u8, T::MaxTokenSymbolLength>,
 			contest_end_date: BoundedVec<u8, T::MaxContestEndDateLength>,
@@ -138,14 +148,16 @@ pub mod pallet {
 			let who = ensure_signed(origin)?;
 
 			Self::validate_contest_new(
+				who.clone(),
 				contest_id.clone(),
 				title.clone(),
 				prize_token_id.clone(),
+				prize_token_amount.clone(),
 				prize_token_winner.clone(),
 				token_symbol.clone(),
 				contest_end_date.clone(),
 				description.clone()
-			);
+			)?;
 
 			let contest = Contests::<T> {
 				contest_id: contest_id.clone(),
@@ -159,9 +171,17 @@ pub mod pallet {
 				description: description.clone()
 			};
 			
+			T::Assets::transfer(
+				prize_token_id, 
+				&who, 
+				&T::PalletId::get().into_account_truncating(),
+				prize_token_amount,
+				false	
+			)?;
+
 			ContestsMap::<T>::insert(contest_id, contest);
 
-			Self::deposit_event(Event::<T>::ContestCreted { who } );
+			Self::deposit_event(Event::<T>::ContestCreted { who, contest_id, title } );
 
 			Ok(())
 		}
@@ -169,10 +189,14 @@ pub mod pallet {
 		#[pallet::call_index(2)]
 		#[pallet::weight(0)]
 		pub fn update_contest(
-			origin: OriginFor<T>
+			origin: OriginFor<T>,
+			contest_id: u32,
+
 		) -> DispatchResult {
 			let who = ensure_signed(origin)?;
 
+
+			
 			Ok(())
 		}
 
@@ -210,9 +234,11 @@ pub mod pallet {
 
 impl<T: Config> Pallet<T> {
 	fn validate_contest_new(
+		who: T::AccountId,
 		contest_id: u32,
 		title: BoundedVec<u8, T::MaxTitleLength>,
 		prize_token_id: AssetIdOf<T>,
+		prize_token_amount: AssetBalanceOf<T>,
 		prize_token_winner: u32,
 		token_symbol: BoundedVec<u8, T::MaxTokenSymbolLength>,
 		contest_end_date: BoundedVec<u8, T::MaxContestEndDateLength>,
@@ -221,10 +247,13 @@ impl<T: Config> Pallet<T> {
 
 		ensure!(ContestsMap::<T>::contains_key(contest_id.clone()), Error::<T>::ContestIdAlreadyInUse);
 		ensure!(T::Assets::asset_exists(prize_token_id.clone()), Error::<T>::AssetDontExist);
-		ensure!(prize_token_winner < T::MinTokenWinner, Error::<T>::PrizeTokenWinnerTooSmall);
-		ensure!(title.len() > T::MinTitleLength, Error::<T>::TitleTooSmall);
-		ensure!(token_symbol.len() > T::MinTokenSymbolLength, Error::<T>::TokenSymbolTooSmall);
-		ensure!(description.len() > T::MinDescriptionLength, Error::<T>::DescriptionTooSmall);
+		ensure!(prize_token_winner < T::MinTokenWinner::get(), Error::<T>::PrizeTokenWinnerTooSmall);
+		ensure!(title.len() as u32> T::MinTitleLength::get(), Error::<T>::TitleTooSmall);
+		ensure!(token_symbol.len() as u32 > T::MinTokenSymbolLength::get(), Error::<T>::TokenSymbolTooSmall);
+		ensure!(description.len() as u32 > T::MinDescriptionLength::get(), Error::<T>::DescriptionTooSmall);
+		ensure!(prize_token_amount < T::MinTokenAmount::get().into(), Error::<T>::TokenAmountTooSmall);
+		ensure!(T::Assets::balance(prize_token_id, &who) >= T::MinTokenAmount::get().into(), Error::<T>::AssetBalanceInsufficient);
+		
 		// Need to finish the contest_end_date validation yet
 
 		Ok(())
